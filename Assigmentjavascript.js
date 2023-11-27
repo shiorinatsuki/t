fba7b51d57041a19c93676f48be42a051fa548f3
shiorinatsuki <152084437+shiorinatsuki@users.noreply.github.com>
2023-11-27 13:02:19 +0800

    Add files via upload

---
+++ Assigmentjavascript.js
@@ -0,0 +1,20 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+function addHover(element) {
+    element.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.3)';
+}
+
+function removeHover(element) {
+    element.style.boxShadow = 'none';
+}
+
+function validateForm() {
+    // Your form validation logic goes here
+    // For a simple example, you can display an alert
+    alert('Form submitted (validation not implemented in this example).');
+}
+
+
